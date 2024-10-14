ed8e30159fc36125d417e61d75980c85a0678028
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 16:38:22 -0400

    Added 2D Dynamic Programming questions.

--- src/TwoDynamicProgramming.java
+++ src/TwoDynamicProgramming.java
@@ -1,2 +1,31 @@
 public class TwoDynamicProgramming {
+
+
+    // There is an m x n grid where you are allowed to move either down or to the right at any point in time.
+
+    // Given the two integers m and n, return the number of possible unique paths that can be taken from the
+    // top-left corner of the grid (grid[0][0]) to the bottom-right corner (grid[m - 1][n - 1]).
+
+    // You may assume the output will fit in a 32-bit integer.
+
+    public int uniquePaths(int m, int n) {
+
+        return m;
+    }
+
+    // Given two strings text1 and text2, return the length of the longest common subsequence between the
+    // two strings if one exists, otherwise return 0.
+
+    // A subsequence is a sequence that can be derived from the
+    // given sequence by deleting some or no elements without changing the relative order of the remaining characters.
+
+    // For example, "cat" is a subsequence of "crabt".
+    // A common subsequence of two strings is a subsequence that exists in both strings.
+
+    public int longestCommonSubsequence(String text1, String text2) {
+
+        return 0;
+    }
+
+
 }
