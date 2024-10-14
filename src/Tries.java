5b4c2ed021ac51da6258a79fbc3154858dcf2499
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 16:20:25 -0400

    Added tries questions

--- src/Tries.java
+++ src/Tries.java
@@ -1,2 +1,78 @@
+import java.util.List;
+
 public class Tries {
+
+
+    // A prefix tree (also known as a trie) is a tree data structure used to efficiently store and
+    // retrieve keys in a set of strings.
+    // Some applications of this data structure include auto-complete and spell checker systems.
+
+
+    // Implement the PrefixTree class:
+
+    //PrefixTree() Initializes the prefix tree object.
+    //void insert(String word) Inserts the string word into the prefix tree.
+    //boolean search(String word) Returns true if the string word is in the prefix tree (i.e., was inserted before), and false otherwise.
+    //boolean startsWith(String prefix)
+    // Returns true if there is a previously inserted string word that has the prefix [prefix]
+    // and false otherwise.
+
+
+        // TODO : REMOVE COMMENTS BELOW
+
+    // public PrefixTree() {
+
+    //    }
+
+    public void insert(String word) {
+
+    }
+
+    public boolean search(String word) {
+
+        return false;
+    }
+
+    public boolean startsWith(String prefix) {
+
+        return false;
+    }
+
+    // Design a data structure that supports adding new words and searching for existing words.
+
+    // Implement the WordDictionary class:
+
+    // void addWord(word) Adds word to the data structure.
+    // bool search(word) Returns true if there is any string in the data structure that matches word or false otherwise.
+    // word may contain dots '.' where dots can be matched with any letter.
+
+
+
+    // TODO :  REMOVE COMMENTS BELOW
+
+    // public WordDictionary() {
+
+    // }
+
+    public void addWord(String word) {
+
+    }
+
+    public boolean search2(String word) {
+
+        return false;
+    }
+
+    // Given a 2-D grid of characters board and a list of strings words, return all words that are present in the grid.
+
+    // For a word to be present it must be possible to form the word with a path in the board with
+    // horizontally or vertically neighboring cells. The same cell may not be used more than once in a word.
+
+    public List<String> findWords(char[][] board, String[] words) {
+
+        return List.of();
+    }
+
+
+
 }
