800ce8e773cd175b317ee29cb4adcb78d2899ba7
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 13:43:59 -0400

    file changes

---
+++ src/ArraysAndHashing.java
@@ -0,0 +1,51 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class ArraysAndHashing {
+
+    public boolean hasDuplicate(int[] nums) {
+
+        HashSet<Integer> x = new HashSet<Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (x.contains(nums[i])) {
+
+                return true;
+            }
+
+            x.add(nums[i]);
+        }
+
+        return false;
+
+    }
+
+
+    public boolean isAnagram(String s, String t) {
+
+        if (s.length() != t.length()) {
+
+            return false;
+        }
+
+        char[] x = s.toCharArray();
+        char[] y = t.toCharArray();
+
+        Arrays.sort(x);
+        Arrays.sort(y);
+
+
+        if (Arrays.equals(x, y)) {
+
+            return true;
+        }
+
+
+        return false;
+
+
+    }
+
+
+}
--- src/Blind75.java
+++ src/Blind75.java
@@ -4,50 +4,13 @@ import java.util.HashSet;
 public class Blind75 {
 
     // Starting the NeetCode Blind 75.
-    // Format code with ⌘+ ⌥ + L
-
-    public boolean hasDuplicate(int[] nums) {
-
-        HashSet<Integer> x = new HashSet<Integer>();
-
-        for (int i = 0; i < nums.length; i++) {
-
-            if (x.contains(nums[i])) {
-
-                return true;
-            }
-
-            x.add(nums[i]);
-        }
-
-        return false;
-
-    }
+    // Format code with ⌘ + ⌥ + L
+    // Open terminal with ⌥ + F12
 
 
-    public boolean isAnagram(String s, String t) {
+    public static void main(String[] args) {
 
-        if (s.length() != t.length()) {
-
-            return false;
-        }
-
-        char[] x = s.toCharArray();
-        char[] y = t.toCharArray();
-
-        Arrays.sort(x);
-        Arrays.sort(y);
-
-
-        if (Arrays.equals(x, y)) {
-
-            return true;
-        }
-
-
-        return false;
-
-
+        System.out.println("Hello World!");
     }
 
 }
