5c6d38fa398161e24a442c3d164f2be44a12403a
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 16:47:48 -0400

    Added math and geometry questions

--- src/MathAndGeometry.java
+++ src/MathAndGeometry.java
@@ -1,2 +1,38 @@
+import java.util.List;
+
 public class MathAndGeometry {
+
+
+
+
+
+    // Given a square n x n matrix of integers matrix, rotate it by 90 degrees clockwise.
+
+    // You must rotate the matrix in-place. Do not allocate another 2D matrix and do the rotation.
+
+    public void rotate(int[][] matrix) {
+
+    }
+
+
+    // Given an m x n matrix of integers matrix,
+    // return a list of all elements within the matrix in spiral order.
+
+
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+        return List.of();
+    }
+
+
+    // Given an m x n matrix of integers matrix,
+    // if an element is 0, set its entire row and column to 0's.
+
+    // You must update the matrix in-place.
+
+    public void setZeroes(int[][] matrix) {
+
+    }
+
+
 }
