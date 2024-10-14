0bb67dee8a34fbe80d6b50133b07c1cbaa163314
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 13:49:42 -0400

    Added two sum

--- src/ArraysAndHashing.java
+++ src/ArraysAndHashing.java
@@ -3,6 +3,10 @@ import java.util.HashSet;
 
 public class ArraysAndHashing {
 
+
+    // Given an integer array nums, return true
+    // if any value appears more than once in the array, otherwise return false.
+
     public boolean hasDuplicate(int[] nums) {
 
         HashSet<Integer> x = new HashSet<Integer>();
@@ -20,6 +24,13 @@ public class ArraysAndHashing {
         return false;
 
     }
+
+
+    // Given two strings s and t, return true
+    // if the two strings are anagrams of each other, otherwise return false.
+
+    // An anagram is a string that contains the exact same characters as another string
+    // but the order of the characters can be different.
 
 
     public boolean isAnagram(String s, String t) {
@@ -45,6 +56,19 @@ public class ArraysAndHashing {
         return false;
 
 
+    }
+
+    // Given an array of integers nums and an integer target,
+    // return the indices i and j such that nums[i] + nums[j] == target and i != j.
+
+    //You may assume that every input has exactly one pair of indices i and j that satisfy the condition.
+    //Return the answer with the smaller index first.
+
+
+    public int[] twoSum(int[] nums, int target) {
+
+
+        return nums;
     }
 
 
