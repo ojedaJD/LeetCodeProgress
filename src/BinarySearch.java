4e07b50807b35bbb5046f98d3c4a77da66d24316
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 15:33:48 -0400

    Binary search questions added

--- src/BinarySearch.java
+++ src/BinarySearch.java
@@ -1,2 +1,48 @@
 public class BinarySearch {
+
+
+    // You are given an array of length n which was originally sorted in ascending order.
+    // It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+    // [3,4,5,6,1,2] if it was rotated 4 times.
+    // [1,2,3,4,5,6] if it was rotated 6 times.
+
+    // Notice that rotating the array 4 times moves the last four elements of the array to the beginning.
+    // Rotating the array 6 times produces the original array
+    // Assuming all elements in the rotated sorted array nums are unique,
+    // return the minimum element of this array.
+
+    // A solution that runs in O(n) time is trivial,
+    // can you write an algorithm that runs in O(log n) time?
+
+
+
+    public int findMin(int[] nums) {
+
+        return 0;
+    }
+
+    // You are given an array of length n which was originally sorted in ascending order.
+    // It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+
+    // [3,4,5,6,1,2] if it was rotated 4 times.
+    // [1,2,3,4,5,6] if it was rotated 6 times.
+
+    // Given the rotated sorted array nums and an integer target
+    // return the index of target within nums, or -1 if it is not present.
+
+    // You may assume all elements in the sorted rotated array nums are unique.
+    // A solution that runs in O(n) time is trivial,
+
+    // Can you write an algorithm that runs in O(log n) time?
+
+
+    public int search(int[] nums, int target) {
+
+
+        return target;
+    }
+
+
+
+
 }
