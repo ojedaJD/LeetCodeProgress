778febb301d1613ea3e516a792396e3d68663d1a
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 16:40:25 -0400

    Added greedy questions

--- src/Greedy.java
+++ src/Greedy.java
@@ -1,2 +1,28 @@
 public class Greedy {
+
+
+    // Given an array of integers nums,
+    // find the subarray with the largest sum and return the sum.
+
+    // A subarray is a contiguous non-empty sequence of elements within an array.
+    public int maxSubArray(int[] nums) {
+
+
+        return 0;
+    }
+
+
+    // You are given an integer array nums where each element nums[i]
+    // indicates your maximum jump length at that position.
+
+    // Return true if you can reach the last index starting from index 0, or false otherwise.
+
+
+    public boolean canJump(int[] nums) {
+
+        return false;
+    }
+
+
 }
+
