d148181bf1ca5087c6276dd1b75260c5531fb20d
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 14:11:42 -0400

    Added two pointer questions

--- src/TwoPointers.java
+++ src/TwoPointers.java
@@ -1,2 +1,37 @@
+import java.util.List;
+
 public class TwoPointers {
+
+
+    // Given a string s, return true if it is a palindrome, otherwise return false.
+    // A palindrome is a string that reads the same forward and backward.
+    // It is also case-insensitive and ignores all non-alphanumeric characters.
+
+    public boolean isPalindrome(String s) {
+
+        return false;
+    }
+
+
+    // Given an integer array nums,
+    // return all the triplets [nums[i], nums[j], nums[k]] where nums[i] + nums[j] + nums[k] == 0,
+    // and the indices i, j and k are all distinct.
+    // The output should not contain any duplicate triplets.
+    // You may return the output and the triplets in any order.
+
+
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        return List.of();
+    }
+
+    // You are given an integer array heights where heights[i] represents the height of the ith bar
+    // You may choose any two bars to form a container. Return the maximum amount of water a container can store.
+
+    public int maxArea(int[] heights) {
+
+        return 0;
+    }
+
+
 }
