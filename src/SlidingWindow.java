223b3a544ca01732858476b250fbd0d108a0cecd
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 15:28:31 -0400

    Added sliding window questions

--- src/SlidingWindow.java
+++ src/SlidingWindow.java
@@ -1,2 +1,49 @@
 public class SlidingWindow {
+
+
+    // You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+    // You may choose a single day to buy one NeetCoin and
+    // choose a different day in the future to sell it.
+    // Return the maximum profit you can achieve.
+    // You may choose to not make any transactions, in which case the profit would be 0.
+
+    public int maxProfit(int[] prices) {
+
+        return 0;
+    }
+
+    // Given a string s, find the length of the longest substring without duplicate characters.
+    // A substring is a contiguous sequence of characters within a string.
+
+    public int lengthOfLongestSubstring(String s) {
+
+
+        return 0;
+    }
+
+    // You are given a string s consisting of only uppercase english characters and an integer k.
+    // You can choose up to k characters of the string and replace them with any other uppercase English character.
+    // After performing at most k replacements
+    // return the length of the longest substring which contains only one distinct character.
+
+    public int characterReplacement(String s, int k) {
+
+        return k;
+    }
+
+
+    // Given two strings s and t, return the shortest substring of s such that every character in t,
+    // including duplicates, is present in the substring. If such a substring does not exist, return an empty string "".
+    // You may assume that the correct output is always unique.
+
+    public String minWindow(String s, String t) {
+
+        return s;
+    }
+
+
+
+
+
+
 }
