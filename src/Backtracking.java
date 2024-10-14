42e9ef8e68e98191ccfc1e8734060bb5eafa3a22
Elijah Mendez <elijahmendez2022@gmail.com>
2024-10-14 16:14:42 -0400

    Added backtracking questions

--- src/Backtracking.java
+++ src/Backtracking.java
@@ -1,2 +1,26 @@
+import java.util.List;
+
 public class Backtracking {
+
+    // You are given an array of distinct integers nums and a target integer target.
+    // Your task is to return a list of all unique combinations of nums where the chosen numbers sum to target.
+
+    public List<List<Integer>> combinationSum(int[] nums, int target) {
+
+        return List.of();
+    }
+
+
+    // Given a 2-D grid of characters board and a string word, return true if the word is present in the grid, otherwise return false.
+
+    // For the word to be present it must be possible to form it with a path
+    // in the board with horizontally or vertically neighboring cells.
+    // The same cell may not be used more than once in a word.
+
+    public boolean exist(char[][] board, String word) {
+
+        return false;
+    }
+
+
 }
